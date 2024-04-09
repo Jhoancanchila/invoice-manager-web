d4bda3c340e87724084bf5154072c36e6b0f45a9
jhoancanchila <canchila.jhoan@gmail.com>
2024-04-09 18:15:33 -0500

    [ADD]functionlity upload voucher and refactoring components

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,30 +1,16 @@
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
-const Loading = () => {
+const Loading = ({ cells }) => {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        <Skeleton />
-      </th>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
-      <td className="px-6 py-4">
-        <Skeleton />
-      </td>
+      {
+        cells?.map(item =>
+          <th key={item} scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <Skeleton />
+          </th>
+        )
+      }
     </tr>
   )
 }
